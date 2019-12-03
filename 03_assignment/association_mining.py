8b36acdeb017ce8a57959c609b96111968852d5f
lukaavbreht <l.avbreht@gmail.com>
2019-12-03 10:46:32 +0100

    added association mining

--- 03_assignment/association_mining.py
+++ 03_assignment/association_mining.py
@@ -12,8 +12,38 @@ from matplotlib.pyplot import figure, plot, xlabel, ylabel, clim, semilogx, logl
 import pprint
 
 from toolbox.Toolbox_Python02450.Tools.toolbox_02450 import gausKernelDensity
+from toolbox.Toolbox_Python02450.Tools.similarity import binarize2
 from sklearn.neighbors import NearestNeighbors
 
+from apyori import apriori
+
+def mat2transactions(X, labels=[]):
+    T = []
+    for i in range(X.shape[0]):
+        l = np.nonzero(X[i, :])[0].tolist()
+        if labels:
+            l = [labels[i] for i in l]
+        T.append(l)
+    return T
+
+# apyori requires data to be in a transactions format, forunately we just wrote a helper function to do that.
+# T = mat2transactions(X,labels)
+# rules = apriori( T, min_support=0.8, min_confidence=1)
+
+# This function print the found rules and also returns a list of rules in the format:
+# [(x,y), ...]
+# where x -> y
+def print_apriori_rules(rules):
+    frules = []
+    for r in rules:
+        for o in r.ordered_statistics:
+            conf = o.confidence
+            supp = r.support
+            x = ", ".join( list( o.items_base ) )
+            y = ", ".join( list( o.items_add ) )
+            print("{%s} -> {%s}  (supp: %.3f, conf: %.3f)"%(x,y, supp, conf))
+            frules.append( (x,y) )
+    return frules
 
 # Again import data
 airbnb_data = "../data/AB_NYC_2019.csv"
@@ -118,30 +148,33 @@ roomtype_data = raw_data[:, (8)]
 x_rty = np.array(roomtype_data).T
 X_K2, K2_labels = categoric2numeric(x_rty)
 
-
+# binarize continious data
 # Get other parameters and standardise them
-other_params = (9,10,11,12,13,14, 15)
+other_params = (9,10,11,13,14,15)
+atributes_other = [prity_atributes[i] for i in other_params]
 other_data = np.array(raw_data[:, other_params])
-ogiginal_vals = np.array(raw_data[:, other_params])
+Xbin, attributeNamesBin = binarize2(other_data, atributes_other)
 
-N, M = other_data.shape
+# N, M = other_data.shape
+#
+# # To get a shape of (n,1) to use in concatenate (only if we only use one additional parameter
+# if N == 1:
+#     other_data = other_data.reshape((other_data.shape[0], 1))
 
-# To get a shape of (n,1) to use in concatenate (only if we only use one additional parameter
-if N == 1:
-    other_data = other_data.reshape((other_data.shape[0], 1))
 
+# # Standardise the data
 # other_data = other_data - np.ones((N, 1)) * other_data.mean(axis=0)
 # other_data = other_data.astype(np.float64)
 # other_data = other_data * (1 / np.std(other_data, 0))
 
 # Concatenate all of the data int one matrix
 
-X = np.concatenate((X_K1, other_data), axis=1)
-X_labesls = K1_labels + [attributes[i] for i in other_params]
+X = np.concatenate((X_K1, X_K2, Xbin), axis=1)
+X_labesls = K1_labels + K2_labels + attributeNamesBin
 
-# Limit to roomtype that is
+print(X.shape)
 
-# X(YY == np.array([1,0,0]))
-rome_type_1_index = list(map(all,YY == np.array([1,0,0])))
+T = mat2transactions(Xbin,labels=attributeNamesBin)
+rules = apriori(T, min_support=0.3, min_confidence=.6)
+print_apriori_rules(rules)
 
-X = X[rome_type_1_index,:]
